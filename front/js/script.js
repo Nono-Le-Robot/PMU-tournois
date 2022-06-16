1a6f923e430ec1adcea5d13d00349544b6d918cc
nono <renaud.sannier@laposte.net>
2022-06-16 15:24:21 +0200

    nouveau concept
    reccup pnl via leaderboard

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,13 @@
 //donnée utilisateur
 const userData = {
     user_name : "",
-    api_key : "",
-    api_private_key : ""
+    userId : "",
 }
 //faire une vérification RegExp ici pour augmenter la sécurité
 send_btn.addEventListener("click", () =>{
     userData.user_name = user_name.value;
-    userData.api_key = api_key.value;
-    userData.api_private_key = api_private_key.value;
+    userData.userId = u_id.value;
     console.log(userData);
 })
+
+
